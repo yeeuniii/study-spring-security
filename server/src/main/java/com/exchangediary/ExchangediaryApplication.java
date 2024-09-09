7622da28650572a3805d0fb35017a86143b98df6
yeeuniii <anna862700@gmail.com>
2024-09-10 00:20:30 +0900

    build: 서버 환경 구축

---
+++ server/src/main/java/com/exchangediary/ExchangediaryApplication.java
@@ -0,0 +1,13 @@
+package com.exchangediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangediaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangediaryApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/exchangediary/ExchangediaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchangediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangediaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
