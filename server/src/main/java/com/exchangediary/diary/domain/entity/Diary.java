ebf55c30b9b1f4c0e584d8c4a8bc5cd43c04ddbb
yeeuniii <anna862700@gmail.com>
2024-09-10 18:02:25 +0900

    style: import 구문에 와일드카드 제거 (#2)

--- server/src/main/java/com/exchangediary/diary/domain/entity/Diary.java
+++ server/src/main/java/com/exchangediary/diary/domain/entity/Diary.java
@@ -1,6 +1,9 @@
 package com.exchangediary.diary.domain.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
