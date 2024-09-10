86ea50bbf4177a5ecfdf1eee3e01ad418c092c7f
yeeuniii <anna862700@gmail.com>
2024-09-10 18:02:56 +0900

    feat: UploadImage 엔티티 생성 (#2)

---
+++ server/src/main/java/com/exchangediary/diary/domain/entity/UploadImage.java
@@ -0,0 +1,26 @@
+package com.exchangediary.diary.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PRIVATE)
+public class UploadImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String url;
+    private String status;
+}
