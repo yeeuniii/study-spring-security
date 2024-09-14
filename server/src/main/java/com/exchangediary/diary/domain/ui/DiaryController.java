16f712b30b6bf050d5855aca152a4ad21a417338
ppparkta <pppppark2@gmail.com>
2024-09-15 02:52:44 +0900

    style: diary 상세 조회 컨트롤러 설명 추가(#20)

--- server/src/main/java/com/exchangediary/diary/domain/ui/DiaryController.java
+++ server/src/main/java/com/exchangediary/diary/domain/ui/DiaryController.java
@@ -1,8 +1,11 @@
 package com.exchangediary.diary.domain.ui;
 
+import com.exchangediary.diary.domain.ui.dto.response.DiaryDetailResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/diary")
 public class DiaryController {
+    /**
+     * 다이어리 상세 조회
+     * @param diaryId 다이어리ID
+     * @return
+     */
     @GetMapping("/{diaryId}")
-    public ResponseEntity<> viewDatailDiary () {
-
+    public ResponseEntity<DiaryDetailResponse> viewDetail (@PathVariable("diaryId")Long diaryId) {
+        
     }
 }
