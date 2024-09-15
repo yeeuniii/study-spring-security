b1775cef716caa1ae035db29e95b5d1e68d79bcb
yeeuniii <anna862700@gmail.com>
2024-09-16 00:04:08 +0900

    feat: sticker repository에 일기와 연관관계 맺은 스티커 수 구하는 메서드 추가 (#21)

--- server/src/main/java/com/exchangediary/diary/domain/StickerRepository.java
+++ server/src/main/java/com/exchangediary/diary/domain/StickerRepository.java
@@ -4,4 +4,5 @@ import com.exchangediary.diary.domain.entity.Sticker;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StickerRepository extends CrudRepository<Sticker, Long> {
+    int countByDiaryId(long diaryId);
 }
