0066a00ba256ab345f8d5c24be2d56cce31b43da
yeeuniii <anna862700@gmail.com>
2024-09-15 22:25:51 +0900

    style: 생성자 코드 제거 및 @RequiredArgsConstructor 어노테이션 추가(#22)

--- server/src/main/java/com/exchangediary/global/service/StaticImageQueryService.java
+++ server/src/main/java/com/exchangediary/global/service/StaticImageQueryService.java
@@ -4,17 +4,15 @@ import com.exchangediary.global.domain.StaticImageRepository;
 import com.exchangediary.global.domain.entity.StaticImage;
 import com.exchangediary.global.domain.entity.StaticImageType;
 import com.exchangediary.global.ui.dto.response.StickersResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class StaticImageQueryService {
     private final StaticImageRepository staticImageRepository;
-
-    public StaticImageQueryService(StaticImageRepository staticImageRepository) {
-        this.staticImageRepository = staticImageRepository;
-    }
 
     public StickersResponse findStickers() {
         List<StaticImage> stickers = staticImageRepository.findAllByType(StaticImageType.STICKER);
--- server/src/main/java/com/exchangediary/global/ui/StaticImageController.java
+++ server/src/main/java/com/exchangediary/global/ui/StaticImageController.java
@@ -2,17 +2,15 @@ package com.exchangediary.global.ui;
 
 import com.exchangediary.global.service.StaticImageQueryService;
 import com.exchangediary.global.ui.dto.response.StickersResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class StaticImageController {
     private final StaticImageQueryService staticImageQueryService;
-
-    public StaticImageController(StaticImageQueryService staticImageQueryService) {
-        this.staticImageQueryService = staticImageQueryService;
-    }
 
     @GetMapping("stickers")
     public ResponseEntity<StickersResponse> findStickers() {
