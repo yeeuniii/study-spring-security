64bea9f905f8376856bd08cfd8739195293b51e0
jisunggi <wltjd5316@naver.com>
2024-09-17 01:17:06 +0900

    style: 불필요한 주석 제거 (#23)

--- server/src/main/java/com/exchangediary/diary/ui/dto/request/UploadImageRequest.java
+++ server/src/main/java/com/exchangediary/diary/ui/dto/request/UploadImageRequest.java
@@ -1,13 +1,11 @@
 package com.exchangediary.diary.ui.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @NoArgsConstructor
-//@AllArgsConstructor
 @Getter
 @Setter
 public class UploadImageRequest {
