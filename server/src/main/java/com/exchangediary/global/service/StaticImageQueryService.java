c54d678a05e69243f12bbc803db922bb6ec47583
ppparkta <pppppark2@gmail.com>
2024-09-16 14:23:12 +0900

    feat: 기분 목록 조회 서비스 추가 (#39)

--- server/src/main/java/com/exchangediary/global/service/StaticImageQueryService.java
+++ server/src/main/java/com/exchangediary/global/service/StaticImageQueryService.java
@@ -3,6 +3,7 @@ package com.exchangediary.global.service;
 import com.exchangediary.global.domain.StaticImageRepository;
 import com.exchangediary.global.domain.entity.StaticImage;
 import com.exchangediary.global.domain.entity.StaticImageType;
+import com.exchangediary.global.ui.dto.response.MoodsResponse;
 import com.exchangediary.global.ui.dto.response.StickersResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,10 @@ public class StaticImageQueryService {
     public StickersResponse findStickers() {
         List<StaticImage> stickers = staticImageRepository.findAllByType(StaticImageType.STICKER);
         return StickersResponse.from(stickers);
+    }
+
+    public MoodsResponse findMoods() {
+        List<StaticImage> moods = staticImageRepository.findAllByType(StaticImageType.MOOD);
+        return MoodsResponse.from(moods);
     }
 }
