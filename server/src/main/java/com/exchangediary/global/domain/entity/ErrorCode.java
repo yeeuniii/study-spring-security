33b29d4cf5cb92bc27f83e503f2eecb7c67ad283
yeeuniii <anna862700@gmail.com>
2024-09-16 17:47:57 +0900

    feat: 예외 핸들러 생성 - NotFound, 유효하지 않은 인자 예외 (#21)

--- server/src/main/java/com/exchangediary/global/domain/entity/ErrorCode.java
+++ server/src/main/java/com/exchangediary/global/domain/entity/ErrorCode.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+    INVALID_INPUT(400, "입력이 유효하지 않습니다."),
+
     DIARY_NOT_FOUND(404, "일기를 찾을 수 없습니다."),
     STICKER_IMAGE_NOT_FOUND(404, "스티커 이미지를 찾을 수 없습니다.");
 
--- server/src/main/java/com/exchangediary/global/domain/entity/NotFoundException.java
+++ server/src/main/java/com/exchangediary/global/domain/entity/NotFoundException.java
@@ -1,7 +1,9 @@
 package com.exchangediary.global.domain.entity;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class NotFoundException extends RuntimeException {
     private final ErrorCode errorCode;
