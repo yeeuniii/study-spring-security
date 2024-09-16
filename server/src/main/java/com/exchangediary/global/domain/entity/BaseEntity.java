7eb1914d382fabcc13bcb6dfc9fda9d82ee63882
jisunggi <wltjd5316@naver.com>
2024-09-17 03:13:05 +0900

    style: 사용하지 않는 import문 삭제 (#23)

--- server/src/main/java/com/exchangediary/global/domain/entity/BaseEntity.java
+++ server/src/main/java/com/exchangediary/global/domain/entity/BaseEntity.java
@@ -3,7 +3,6 @@ package com.exchangediary.global.domain.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
