b2219df7759c66d4258c51110a16999cb2dcdf60
jisunggi <wltjd5316@naver.com>
2024-09-17 22:36:45 +0900

    style: errorcode 개행추가 (#23)

--- server/src/main/java/com/exchangediary/global/domain/entity/ErrorCode.java
+++ server/src/main/java/com/exchangediary/global/domain/entity/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     DIARY_NOT_FOUND(404, "일기를 찾을 수 없습니다."),
     STICKER_IMAGE_NOT_FOUND(404, "스티커 이미지를 찾을 수 없습니다."),
     MOOD_IMAGE_NOT_FOUND(404, "기분 이미지를 찾을 수 없습니다."),
+
     IMAGE_UPLOAD_ERROR(500, "이미지 업로드 중 오류가 발생했습니다.");
+
     private final int statusCode;
     private final String message;
 }
