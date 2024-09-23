09f64be25003ab4168c6f2549a85d3e6384a4a0a
yeeuniii <anna862700@gmail.com>
2024-09-23 18:05:57 +0900

    feat: 년월 클래스에 현재날짜 넣어주기. 달력에 날짜 그리는 함수 분리 (#87)

--- server/src/main/resources/static/js/draw-calendar.js
+++ server/src/main/resources/static/js/draw-calendar.js
@@ -1,35 +1,43 @@
 const table = document.querySelector("table");
+const year = document.querySelector(".year");
+const month = document.querySelector(".month");
+const trs = Array.from(table.children[0].children).slice(4);
 const today = new Date();
-const todayYear = today.getFullYear();
-const todayMonth = today.getMonth() + 1;
-const firstDay = new Date(todayYear, todayMonth - 1, 1).getDay();
-const lastDate = new Date(todayYear, todayMonth, 0).getDate();
 
-var date = 1;
-var day = firstDay
-var column = 0;
-const trs = Array.from(table.children[0].children).slice(4)
+year.innerText = today.getFullYear();
+month.innerText = today.getMonth() + 1;
+console.log(year.innerText, month.innerText);
 
-while (date <= lastDate) {
-    console.log(`date: ${date}, day: ${day}`);
-    trs[column].children[day].innerHTML = makeCircle(date);
-    date++;
-    day++;
-    if (day == 7) {
-        day = 0;
-        column++;
+drawDateOfCalendar();
+
+function drawDateOfCalendar() {
+    const firstDay = new Date(year.innerText, month.innerText - 1, 1).getDay();
+    const lastDate = new Date(year.innerText, month.innerText, 0).getDate();
+
+    let date = 1;
+    let day = firstDay
+    let column = 0;
+
+    while (date <= lastDate) {
+        trs[column].children[day].innerHTML = makeCircle(date);
+        date++;
+        day++;
+        if (day == 7) {
+            day = 0;
+            column++;
+        }
     }
 }
 
 function makeCircle(date) {
-    return `<a class="day${date}" href="" style="width: 50px; height: 50px; border: 1px solid; border-radius: 50%; font-size: 24px; color: rgba(118, 118, 118, 0.7); display: flex; justify-content: center; align-items: center;">${date}</a>`
+    return `<a class="day${date}" href="" style="width: 50px; height: 50px; border: 1px solid; border-radius: 50%; font-size: 24px; color: rgba(118, 118, 118, 0.7); display: flex; justify-content: center; align-items: center;">${date}</a>`;
 }
 
 
 const days = document.querySelectorAll("a");
 
-Array.from(days).forEach( day => {
-    day.addEventListener("click", clickDayBtn);
+Array.from(days).forEach( date => {
+    date.addEventListener("click", clickDayBtn);
 })
 
 function clickDayBtn(event) {
