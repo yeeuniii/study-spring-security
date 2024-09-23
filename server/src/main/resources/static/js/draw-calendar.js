182a1570d764e94052e26a8da29886550ba008b1
yeeuniii <anna862700@gmail.com>
2024-09-23 18:06:27 +0900

    feat: 화살표 버튼 클릭 이벤트 처리 (#87)

--- server/src/main/resources/static/js/draw-calendar.js
+++ server/src/main/resources/static/js/draw-calendar.js
@@ -29,6 +29,20 @@ function drawDateOfCalendar() {
     }
 }
 
+function clearDate() {
+    let column = 0;
+    let row = 0;
+
+    while (column < 5) {
+        trs[column].children[row].innerText = "";
+        row++;
+        if (row == 7) {
+            column++;
+            row = 0;
+        }
+    }
+}
+
 function makeCircle(date) {
     return `<a class="day${date}" href="" style="width: 50px; height: 50px; border: 1px solid; border-radius: 50%; font-size: 24px; color: rgba(118, 118, 118, 0.7); display: flex; justify-content: center; align-items: center;">${date}</a>`;
 }
@@ -43,4 +57,32 @@ Array.from(days).forEach( date => {
 function clickDayBtn(event) {
     event.preventDefault();
     alert(`${event.target.innerText}일 버튼을 눌렀습니다`);
+}
+
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+
+leftArrow.addEventListener("click", clickLeftArrowButton);
+rightArrow.addEventListener("click", clickRightArrowButton);
+
+function clickLeftArrowButton(event) {
+    event.preventDefault();
+    month.innerText -= 1;
+    if (month.innerText == 0) {
+        month.innerText = 12;
+        year.innerText -= 1;
+    }
+    clearDate();
+    drawDateOfCalendar()
+}
+
+function clickRightArrowButton(event) {
+    event.preventDefault();
+    month.innerText = Number(month.innerText) + 1;
+    if (month.innerText == 13) {
+        month.innerText = 1;
+        year.innerText = Number(year.innerText) + 1;
+    }
+    clearDate();
+    drawDateOfCalendar()
 }
