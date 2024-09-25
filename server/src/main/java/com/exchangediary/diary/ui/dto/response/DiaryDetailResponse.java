d936ddd447ad30528a051cb2ff1f6fae9e6b8ecc
yeeuniii <anna862700@gmail.com>
2024-09-26 03:05:19 +0900

    fix: 업로드 이미지 null 예외 처리 (#96)

--- server/src/main/java/com/exchangediary/diary/ui/dto/response/DiaryDetailResponse.java
+++ server/src/main/java/com/exchangediary/diary/ui/dto/response/DiaryDetailResponse.java
@@ -1,6 +1,7 @@
 package com.exchangediary.diary.ui.dto.response;
 
 import com.exchangediary.diary.domain.entity.Diary;
+import com.exchangediary.diary.domain.entity.UploadImage;
 import com.exchangediary.global.util.DateTimeUtil;
 import lombok.Builder;
 
@@ -13,12 +14,19 @@ public record DiaryDetailResponse(
         String imageApiPath
         ) {
     public static DiaryDetailResponse of (Diary diary) {
-        return DiaryDetailResponse.builder()
+       return DiaryDetailResponse.builder()
                 .diaryId(diary.getId())
                 .createdAt(DateTimeUtil.KOREAN_DATE_FORMAT.format(diary.getCreatedAt()))
                 .content(diary.getContent())
                 .moodLocation(diary.getMoodLocation())
-                .imageApiPath("/api/diary/upload-image/" + diary.getUploadImage().getId())
+                .imageApiPath(getImageApiPath(diary.getUploadImage()))
                 .build();
+    }
+
+    private static String getImageApiPath(UploadImage uploadImage) {
+        if (uploadImage == null) {
+            return null;
+        }
+        return "/api/diary/upload-image/" + uploadImage.getId();
     }
 }
