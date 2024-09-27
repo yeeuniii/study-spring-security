1d9b417b466079851a9d652762acf07f1dc55313
yeeuniii <anna862700@gmail.com>
2024-09-27 14:50:53 +0900

    refactor: 요청 body validation 실패 시 예외 핸들러 (#114)

--- server/src/main/java/com/exchangediary/global/exception/ErrorCode.java
+++ server/src/main/java/com/exchangediary/global/exception/ErrorCode.java
@@ -7,15 +7,18 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-
+    // Service Exception
     INVALID_RANGE(HttpStatus.BAD_REQUEST, "유효하지 않은 범위입니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 자원입니다."),
 
+    // Invalid Range
     INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "유효하지 않은 날짜 범위입니다."),
 
+    // Not Found
     DIARY_NOT_FOUND(HttpStatus.NOT_FOUND, "일기를 찾을 수 없습니다."),
     UPLOAD_IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "일기 업로드 이미지를 찾을 수 없습니다."),
 
+    // etc
     FAILED_UPLOAD_IMAGE(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드 중 오류가 발생했습니다.");
 
     private final HttpStatus statusCode;
--- server/src/main/java/com/exchangediary/global/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/exchangediary/global/exception/GlobalExceptionHandler.java
@@ -1,29 +1,34 @@
 package com.exchangediary.global.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Objects;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-//    public ResponseEntity<ErrorResponse> handleInvalidArgumentException(MethodArgumentNotValidException exception) {
-//        return ;
-//    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ApiErrorResponse handleInvalidArgumentException(MethodArgumentNotValidException exception) {
+        try {
+            return ApiErrorResponse.from(Objects.requireNonNull(exception.getFieldError()));
+        } catch (NullPointerException e) {
+            return ApiErrorResponse.from(HttpStatus.BAD_REQUEST);
+        }
+    }
 
     @ExceptionHandler(InvalidRangeException.class)
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleInvalidRangeException(InvalidDateException exception) {
-        return ErrorResponse.from(exception.getErrorCode(), exception.getValue());
+    public ApiErrorResponse handleInvalidRangeException(InvalidDateException exception) {
+        return ApiErrorResponse.of(exception.getErrorCode(), exception.getValue());
     }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFoundException(NotFoundException exception) {
-        return ErrorResponse.from(exception.getErrorCode(), exception.getValue());
+    public ApiErrorResponse handleNotFoundException(NotFoundException exception) {
+        return ApiErrorResponse.of(exception.getErrorCode(), exception.getValue());
     }
 }
