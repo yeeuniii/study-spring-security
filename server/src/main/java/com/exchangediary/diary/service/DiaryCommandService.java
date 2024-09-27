1a46c7c3f370ba3e79dfc8a76a4f8e0558269967
yeeuniii <anna862700@gmail.com>
2024-09-28 02:21:17 +0900

    fix: 일기 하루에 여러 번 작성 가능한 문제 해결 (#117)

--- server/src/main/java/com/exchangediary/diary/service/DiaryCommandService.java
+++ server/src/main/java/com/exchangediary/diary/service/DiaryCommandService.java
@@ -4,6 +4,7 @@ import com.exchangediary.diary.domain.DiaryRepository;
 import com.exchangediary.diary.domain.entity.Diary;
 import com.exchangediary.diary.domain.entity.UploadImage;
 import com.exchangediary.diary.ui.dto.request.DiaryRequest;
+import com.exchangediary.global.exception.DiaryDuplicateException;
 import com.exchangediary.global.exception.FailedImageUploadException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,13 +23,23 @@ public class DiaryCommandService {
     private final UploadImageService uploadImageService;
 
     public Diary createDiary(DiaryRequest diaryRequest, MultipartFile file) {
+        checkTodayDiaryExistent();
+
         try {
             UploadImage uploadImage = uploadImageService.saveUploadImage(file);
-
             Diary diary = Diary.of(diaryRequest, uploadImage);
             return diaryRepository.save(diary);
         } catch (IOException e) {
             throw new FailedImageUploadException(file.getOriginalFilename());
+        }
+    }
+
+    private void checkTodayDiaryExistent() {
+        LocalDate today = LocalDate.now();
+        Optional<Long> todayDiary = diaryRepository.findIdByDate(today.getYear(), today.getMonthValue(), today.getDayOfMonth());
+
+        if (todayDiary.isPresent()) {
+            throw new DiaryDuplicateException(today.toString());
         }
     }
 }
