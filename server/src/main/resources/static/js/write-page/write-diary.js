54e283480308994064baeca17d44095fe01fc325
yeeuniii <anna862700@gmail.com>
2024-09-30 03:50:05 +0900

    feat: 일기 작성 API 연동 (#88)

--- server/src/main/resources/static/js/write-page/write-diary.js
+++ server/src/main/resources/static/js/write-page/write-diary.js
@@ -1,8 +1,32 @@
 const date = document.querySelector(".date")
+const writeBtn = document.querySelector(".write-btn")
 const today = new Date();
 
-function init() {
-    date.innerText = `${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}`;
-}
+date.innerText = `${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}`;
 
-init();
+console.log(writeBtn);
+writeBtn.addEventListener("click", writeDiary);
+
+function writeDiary() {
+    console.log(date);
+    const content = document.querySelector(".diary-content").value;
+    const mood = document.querySelector(".mood-btn").children[0];
+    const moodLocation = mood.src.substring(mood.src.indexOf("/images"));
+    const uploadImage = null;
+
+    var formData = new FormData();
+
+    const json =  JSON.stringify({
+        content: content,
+        moodLocation: moodLocation
+    });
+    formData.append("data", new Blob([json], {type: "application/json"}));
+    formData.append("file", uploadImage);
+
+    fetch("/api/diary", {
+        method: "post",
+        body: formData
+    })
+        .then(response => response.headers.get("location"))
+        .then(location => window.location.href = location);
+}
