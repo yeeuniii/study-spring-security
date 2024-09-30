a20e6199494550460825ea9f9024c0d450186f1c
yeeuniii <anna862700@gmail.com>
2024-10-01 00:52:12 +0900

    test: 그룹 코드 생성 단위 테스트 작성 (#104)

---
+++ server/src/test/java/com/exchangediary/group/GroupCodeServiceTest.java
@@ -0,0 +1,27 @@
+package com.exchangediary.group;
+
+import com.exchangediary.group.service.GroupCodeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Base64;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class GroupCodeServiceTest {
+    @InjectMocks
+    private GroupCodeService groupCodeService;
+
+    @Test
+    void 그룹_코드_생성() {
+        String groupName = "버디즈";
+
+        String encodedCode = groupCodeService.generateCode(groupName);
+
+        String code = new String(Base64.getDecoder().decode(encodedCode));
+        assertThat(code.substring(0, 3)).isEqualTo(groupName);
+    }
+}
