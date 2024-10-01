01262e5ecd8a87f596823240ab6d203905fdf612
yeeuniii <anna862700@gmail.com>
2024-10-01 17:20:14 +0900

    feat: 일기 단일 조회 컨트롤러 추가 (#113)

--- server/src/main/java/com/exchangediary/diary/ui/DiaryController.java
+++ server/src/main/java/com/exchangediary/diary/ui/DiaryController.java
@@ -1,7 +1,9 @@
 package com.exchangediary.diary.ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +12,11 @@ public class DiaryController {
     @GetMapping
     public String writePage() {
         return "write-page";
+    }
+
+    @GetMapping("/{diaryId}")
+    public String viewDiary(Model model, @PathVariable Long diaryId) {
+        model.addAttribute("diaryId", diaryId);
+        return "view-diary";
     }
 }
