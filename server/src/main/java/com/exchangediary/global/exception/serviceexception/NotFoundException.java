9646022283f8705c29e1316fcc8cc1e15a457cad
yeeuniii <anna862700@gmail.com>
2024-10-02 22:49:56 +0900

    fix: not found 예외 클래스 생성자 public으로 수정 (#146)

--- server/src/main/java/com/exchangediary/global/exception/serviceexception/NotFoundException.java
+++ server/src/main/java/com/exchangediary/global/exception/serviceexception/NotFoundException.java
@@ -3,7 +3,7 @@ package com.exchangediary.global.exception.serviceexception;
 import com.exchangediary.global.exception.ErrorCode;
 
 public class NotFoundException extends ServiceException {
-    private NotFoundException(ErrorCode errorCode, String message, String value) {
+     public NotFoundException(ErrorCode errorCode, String message, String value) {
         super(errorCode, message, value);
     }
 }
