1d1889a4a90acb99f0a8ee5e4463e47f7f8a4ffb
yeeuniii <anna862700@gmail.com>
2024-10-02 22:49:41 +0900

    style: 예외 클래스 메시지 빈 문자열 처리 (#146)

--- server/src/main/java/com/exchangediary/global/exception/serviceexception/ServiceException.java
+++ server/src/main/java/com/exchangediary/global/exception/serviceexception/ServiceException.java
@@ -3,13 +3,15 @@ package com.exchangediary.global.exception.serviceexception;
 import com.exchangediary.global.exception.ErrorCode;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class ServiceException extends RuntimeException {
     private final ErrorCode errorCode;
     private final String value;
 
     public ServiceException(ErrorCode errorCode, String message, String value) {
-        super(message == null ? errorCode.getMessage() : message);
+        super(Objects.equals(message, "") ? errorCode.getMessage() : message);
         this.errorCode = errorCode;
         this.value = value;
     }
