7f331e8af559fc6ec82395ac347315be12248900
yeeuniii <anna862700@gmail.com>
2024-10-03 03:00:05 +0900

    feat: 그룹 생성 API 컨트롤러 추가 (#140)

--- server/src/main/java/com/exchangediary/group/ui/ApiGroupController.java
+++ server/src/main/java/com/exchangediary/group/ui/ApiGroupController.java
@@ -1,7 +1,9 @@
 package com.exchangediary.group.ui;
 
 import com.exchangediary.group.service.GroupCodeService;
+import com.exchangediary.group.service.GroupCommandService;
 import com.exchangediary.group.ui.dto.request.GroupCodeRequest;
+import com.exchangediary.group.ui.dto.request.GroupNameRequest;
 import com.exchangediary.group.ui.dto.response.GroupIdResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +13,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/groups")
 public class ApiGroupController {
+    private final GroupCommandService groupCommandService;
     private final GroupCodeService groupCodeService;
+
+    @PostMapping
+    public ResponseEntity<GroupIdResponse> createGroup(
+            @RequestBody @Valid GroupNameRequest request
+    ) {
+        GroupIdResponse groupIdResponse = groupCommandService.createGroup(request.groupName());
+        return ResponseEntity
+                .created(URI.create("/api/groups/" + groupIdResponse.groupId()))
+                .body(groupIdResponse);
+    }
 
     @PostMapping("/code/verify")
     public ResponseEntity<GroupIdResponse> verifyGroupCode(
