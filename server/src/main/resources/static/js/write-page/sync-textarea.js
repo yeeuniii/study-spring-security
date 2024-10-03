baa6ba5b83c30980053c49ef54de9f91f6f81f86
yeeuniii <anna862700@gmail.com>
2024-10-04 03:03:35 +0900

    style: 수정된 디자인 반영하면서 불필요해진 코드 제거 (#88)

--- server/src/main/resources/static/js/write-page/sync-textarea.js
+++ server/src/main/resources/static/js/write-page/sync-textarea.js
@@ -1,18 +1,11 @@
 const DEFAULT_HEIGHT = 50;
 const textarea = document.querySelector(".diary-content");
-const textBtn = document.querySelector(".text-btn");
 
 textarea.addEventListener("input", syncHeight);
-textBtn.addEventListener("click", clickTextBtn);
 
 function syncHeight(event) {
     const target = event.target;
 
     target.style.height = `${DEFAULT_HEIGHT}px`;
     target.style.height = `${target.scrollHeight - 4}px`;
-}
-
-function clickTextBtn() {
-    closeModal();
-    textarea.focus();
 }
