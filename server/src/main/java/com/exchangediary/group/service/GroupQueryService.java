ed4bbff7590b079ea79df9a989adafc59657b3b6
jisunggi <wltjd5316@naver.com>
2024-10-04 00:11:35 +0900

    feature: 닉네임 유효성 검사 서비스 구현 (#143)

--- server/src/main/java/com/exchangediary/group/service/GroupQueryService.java
+++ server/src/main/java/com/exchangediary/group/service/GroupQueryService.java
@@ -2,9 +2,11 @@ package com.exchangediary.group.service;
 
 import com.exchangediary.global.exception.ErrorCode;
 import com.exchangediary.global.exception.serviceexception.ConfilctException;
+import com.exchangediary.global.exception.serviceexception.DuplicateException;
 import com.exchangediary.global.exception.serviceexception.NotFoundException;
 import com.exchangediary.group.domain.GroupRepository;
 import com.exchangediary.group.domain.entity.Group;
+import com.exchangediary.group.ui.dto.response.GroupNicknameResponse;
 import com.exchangediary.group.ui.dto.response.GroupProfileResponse;
 import com.exchangediary.member.domain.MemberRepository;
 import com.exchangediary.member.domain.entity.Member;
@@ -41,5 +43,28 @@ public class GroupQueryService {
                     String.valueOf(numberOfMembers)
                     );
         }
+    }
+
+    public GroupNicknameResponse verifyNickname(Long groupId, String nickname) {
+        Group group = groupRepository.findById(groupId)
+                .orElseThrow(() -> new NotFoundException(
+                        ErrorCode.DIARY_NOT_FOUND,
+                        "",
+                        String.valueOf(groupId))
+                );
+        boolean verification = isNicknameDuplicate(groupId, nickname);
+        return GroupNicknameResponse.from(verification);
+    }
+
+    private boolean isNicknameDuplicate(Long groupId, String nickname) {
+        List<Member> members = memberRepository.findAllByGroupId(groupId);
+        if (members.stream().anyMatch(member -> member.getNickname().equals(nickname))) {
+            throw new DuplicateException(
+                    ErrorCode.NICKNAME_DUPLICATED,
+                    "",
+                    nickname
+            );
+        }
+        return true;
     }
 }
