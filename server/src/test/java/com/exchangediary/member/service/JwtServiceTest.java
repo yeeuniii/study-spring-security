46d97841e6fdc001dde47aa2e705e1090d92eb0a
yeeuniii <anna862700@gmail.com>
2024-10-07 16:17:21 +0900

    test: jwt 토큰 생성 및 검증 서비스 테스트 작성 (#152)

---
+++ server/src/test/java/com/exchangediary/member/service/JwtServiceTest.java
@@ -0,0 +1,23 @@
+package com.exchangediary.member.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class JwtServiceTest {
+    @Autowired
+    private JwtService jwtService;
+
+    @Test
+    void 토큰_발급_성공() {
+        Long memberId = 1L;
+        String token = jwtService.generateToken(memberId);
+
+        Long result = jwtService.extractMemberId(token);
+
+        assertThat(result).isEqualTo(memberId);
+    }
+}
