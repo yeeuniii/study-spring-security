26cd9a9210144b458a596596a2c11ff1083776fd
yeeuniii <anna862700@gmail.com>
2024-10-08 02:53:27 +0900

    feat: web mvc configuration 생성 및 interceptor 추가 메서드 오버라이딩 (#152)

---
+++ server/src/main/java/com/exchangediary/global/config/web/WebConfig.java
@@ -0,0 +1,23 @@
+package com.exchangediary.global.config.web;
+
+import com.exchangediary.global.config.web.interceptor.JwtAuthenticationInterceptor;
+import com.exchangediary.member.domain.MemberRepository;
+import com.exchangediary.member.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JwtAuthenticationInterceptor(jwtService, memberRepository))
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/kakao/callback");
+    }
+}
