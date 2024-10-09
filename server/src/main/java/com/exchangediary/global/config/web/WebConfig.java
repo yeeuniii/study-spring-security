e6459b076983d9881d94427c54c3c5c751f9907e
yeeuniii <anna862700@gmail.com>
2024-10-10 02:07:36 +0900

    style: 불필요한 어노테이션 제거 (#174)

--- server/src/main/java/com/exchangediary/global/config/web/WebConfig.java
+++ server/src/main/java/com/exchangediary/global/config/web/WebConfig.java
@@ -5,12 +5,10 @@ import com.exchangediary.member.domain.MemberRepository;
 import com.exchangediary.member.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("!test")
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final JwtService jwtService;
