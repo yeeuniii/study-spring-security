e60d0d13f4e639cb09bf1aab49d1ef31095b1b76
yeeuniii <anna862700@gmail.com>
2024-10-09 14:38:02 +0900

    test: base test 클래스 생성 (#173)

---
+++ server/src/test/java/com/exchangediary/BaseTest.java
@@ -0,0 +1,49 @@
+package com.exchangediary;
+
+import com.exchangediary.member.domain.MemberRepository;
+import com.exchangediary.member.domain.entity.Member;
+import com.exchangediary.member.service.JwtService;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"classpath:truncate.sql"}, executionPhase = BEFORE_TEST_METHOD)
+public class BaseTest {
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private JwtService jwtService;
+    protected Member member;
+    protected String token;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        init();
+    }
+
+    protected void init() {
+        this.member = createMember();
+        memberRepository.save(this.member);
+        this.token = getToken();
+    }
+
+    private Member createMember() {
+        return Member.builder()
+                .kakaoId(1L)
+                .orderInGroup(0)
+                .build();
+    }
+
+    private String getToken() {
+        return jwtService.generateToken(member.getId());
+    }
+}
