f6a9363a4d1232e2a629af6dc96be92524415827
jisunggi <wltjd5316@naver.com>
2024-10-12 15:55:21 +0900

    test: 일기 작성 api 예외시 테스트 코드 추가 (#111)

--- server/src/test/java/com/exchangediary/diary/api/DiaryCreateApiTest.java
+++ server/src/test/java/com/exchangediary/diary/api/DiaryCreateApiTest.java
@@ -3,6 +3,7 @@ package com.exchangediary.diary.api;
 import com.exchangediary.ApiBaseTest;
 import com.exchangediary.diary.domain.DiaryRepository;
 import com.exchangediary.diary.domain.entity.Diary;
+import com.exchangediary.global.exception.ErrorCode;
 import com.exchangediary.group.domain.GroupRepository;
 import com.exchangediary.group.domain.entity.Group;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -18,6 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 class DiaryCreateApiTest extends ApiBaseTest {
     private static final String API_PATH = "/api/groups/%d/diaries";
@@ -58,6 +60,38 @@ class DiaryCreateApiTest extends ApiBaseTest {
         assertThat(newDiary.getMember().getId()).isEqualTo(member.getId());
         assertThat(newDiary.getContent()).isEqualTo(data.get("content"));
         assertThat(newDiary.getMoodLocation()).isEqualTo(data.get("moodLocation"));
+    }
+
+    @Test
+    void 일기_작성_실패_오늘_이미_작성() throws JsonProcessingException {
+        Group group = createGroup();
+        groupRepository.save(group);
+        Diary diary = createDiary(group);
+        diaryRepository.save(diary);
+        Map<String, String> data = new HashMap<>();
+        data.put("content", "buddies");
+        data.put("moodLocation", "/images/sad.png");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonData = objectMapper.writeValueAsString(data);
+
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                .multiPart("data", jsonData, "application/json")
+                .multiPart("file", new File("src/test/resources/images/test.jpg"), "image/png")
+                .cookie("token", token)
+                .when().post(String.format(API_PATH, group.getId()))
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("message", equalTo(ErrorCode.DIARY_DUPLICATED.getMessage()));
+    }
+
+    private Diary createDiary(Group group) {
+        return Diary.builder()
+                .content("하이하이")
+                .moodLocation("/images/write-page/emoji/sleepy.svg")
+                .group(group)
+                .build();
     }
 
     private Group createGroup() {
