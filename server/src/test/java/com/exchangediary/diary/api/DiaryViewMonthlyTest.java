5fcefd90ad48045d9e31b3925e8f6c1571e675cd
jisunggi <wltjd5316@naver.com>
2024-10-12 16:21:00 +0900

    test: 달력형 조회 api 예외시 테스트 코드 추가, 성공 테스트 group 추가 (#111)

--- server/src/test/java/com/exchangediary/diary/api/DiaryViewMonthlyTest.java
+++ server/src/test/java/com/exchangediary/diary/api/DiaryViewMonthlyTest.java
@@ -4,6 +4,7 @@ import com.exchangediary.ApiBaseTest;
 import com.exchangediary.diary.domain.DiaryRepository;
 import com.exchangediary.diary.domain.entity.Diary;
 import com.exchangediary.diary.ui.dto.response.DiaryMonthlyResponse;
+import com.exchangediary.global.exception.ErrorCode;
 import com.exchangediary.group.domain.GroupRepository;
 import com.exchangediary.group.domain.entity.Group;
 import io.restassured.RestAssured;
@@ -12,7 +13,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 class DiaryViewMonthlyTest extends ApiBaseTest {
     private static final String API_PATH = "/api/groups/%d/diaries/monthly";
@@ -24,9 +28,13 @@ class DiaryViewMonthlyTest extends ApiBaseTest {
     @Test
     void 달력형_일기_조회() {
         Group group = createGroup();
+        Group otherGroup = createGroup();
         groupRepository.save(group);
+        groupRepository.saveAll(Arrays.asList(group, otherGroup));
         Diary diary = createDiary(group);
-        diaryRepository.save(diary);
+        Diary diary2 = createDiary(group);
+        Diary otherDiary = createDiary(otherGroup);
+        diaryRepository.saveAll(Arrays.asList(diary, diary2, otherDiary));
 
         DiaryMonthlyResponse response = RestAssured
                 .given().log().all()
@@ -41,7 +49,24 @@ class DiaryViewMonthlyTest extends ApiBaseTest {
 
         assertThat(response.year()).isEqualTo(diary.getCreatedAt().getYear());
         assertThat(response.month()).isEqualTo(diary.getCreatedAt().getMonth().getValue());
-        assertThat(response.days()).hasSize(1);
+        assertThat(response.days()).hasSize(2);
+    }
+
+    @Test
+    void 달력형_일기_조회_실패() {
+        Group group = createGroup();
+        groupRepository.save(group);
+
+        RestAssured
+                .given().log().all()
+                .queryParam("year", "2024")
+                .queryParam("month", "13")
+                .contentType(ContentType.JSON)
+                .cookie("token", token)
+                .when().get(String.format(API_PATH, group.getId()))
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("message", equalTo(ErrorCode.INVALID_DATE.getMessage()));
     }
 
     private Group createGroup() {
