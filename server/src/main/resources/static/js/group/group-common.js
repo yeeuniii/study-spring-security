a5ac9a60a5ac1e1e2169fd76fac525f68c26d7c8
박주한 <147506055+redparkchu@users.noreply.github.com>
2024-10-13 00:35:56 +0000

    feat: 스텝별 흐름 구현 (#130)

--- server/src/main/resources/static/js/group/group-common.js
+++ server/src/main/resources/static/js/group/group-common.js
@@ -1,11 +1,48 @@
 const note_body = document.querySelector(".note-body");
+const step_bar = document.querySelector(".step-bar");
+const stepIcons = Array.from(step_bar.children);
+const steps = {
+    1: drawStep1,
+    2: drawStep1,
+    3: drawStep1,
+    4: drawStep1,
+    5: drawStep1
+}
 
 var currentStep = 1;
 
-function changeStep() {
-
+function nextStep() {
+    addStepIcon();
+    removeStep(-1);
+    drawStep(currentStep, "next");
 }
 
+function addStepIcon() {
+    currentStep += 1;
+    const stepIcon = stepIcons.find((stepIcon) => !stepIcon.classList.contains("fill"));
+    stepIcon.classList.add("fill");
+}
 
+function removeStep(direction) {
+    const step_content = note_body.children[0];
+    step_content.style.transform = `translateX(${100 * direction}%)`;
+    setTimeout(() => step_content.remove(), 300);
+}
 
-drawStep1();
+function drawStep(stepNumber, direction) {
+    setTimeout(() => steps[stepNumber](direction), 350);
+}
+
+function prevStep() {
+    deleteStepIcon();
+    removeStep(1);
+    drawStep(currentStep, "prev");
+}
+
+function deleteStepIcon() {
+    currentStep -= 1;
+    const stepIcon = stepIcons.findLast((stepIcon) => stepIcon.classList.contains("fill"));
+    stepIcon.classList.remove("fill");
+}
+
+drawStep(currentStep);
